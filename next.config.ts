d3f40b3c12d0f29216fb0147ca175c720dcd3505
unknown <abhay.kaushal@yutrp.com>
2026-04-02 10:59:17 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Disables ESLint during builds
-  },
   experimental: {
     // You can keep other experimental flags here
   },
